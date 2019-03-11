9c1d5c9c2eb6d83f88fadd7ce675831cf282f2d6
RatarovDaniil <daniil.ratarov@gmail.com>
2019-03-11 18:13:34 +0100

    adding traverse_directory_tree.py and traverse_directory_tree.csv, updating convert_time.py

--- tasks/convert_time.py
+++ tasks/convert_time.py
@@ -10,25 +10,25 @@ parser.add_argument('input', help='path to the input file')
 args = parser.parse_args()
 
 def convert(file, zone='UTC'):
-	"""
-	input:
-		file: str, path to .h5 file
-		zone: 'UTC' or 'CERN'
-	output:
-		return: datetime object, converted UNIX time
-	"""
+    """
+    input:
+        file: str, path to .h5 file
+        zone: 'UTC' or 'CERN'
+    output:
+        return: datetime object, converted UNIX time
+    """
 
-	# get time and convert to seconds
-	unix_time = int(os.path.basename(file).split('.')[0].split('_')[0]) / 1e10
+    # get time and convert to seconds
+    unix_time = int(os.path.basename(file).split('.')[0].split('_')[0]) / 1e10
 
-	if zone == 'UTC':
-		time = datetime.utcfromtimestamp(unix_time).replace(tzinfo=pytz.utc)
-	elif zone == 'CERN':
-		time = datetime.fromtimestamp(unix_time, pytz.timezone('CET'))
-	else:
-		raise ValueError("zone argument can be 'UTC' or 'CERN'.")
+    if zone == 'UTC':
+        time = datetime.utcfromtimestamp(unix_time).replace(tzinfo=pytz.utc)
+    elif zone == 'CERN':
+        time = datetime.fromtimestamp(unix_time, pytz.timezone('CET'))
+    else:
+        raise ValueError("zone argument can be 'UTC' or 'CERN'.")
 
-	return time 
+    return time 
 
 print('UTC time: ', convert(args.input, zone='UTC'))
 print('CERN local time: ', convert(args.input, zone='CERN'))
---
+++ tasks/traverse_directory_tree.py
@@ -0,0 +1,58 @@
+import h5py
+import pandas as pd
+import argparse
+import os, sys
+
+# parse arguments
+parser = argparse.ArgumentParser(description='Create .csv file with all of the groups and datasets.')
+parser.add_argument('input', help='path to the input file')
+
+args = parser.parse_args()
+
+class Queue():
+    def __init__(self):
+        self.items = []
+
+    def empty(self):
+        return len(self.items) == 0
+
+    def pushBack(self, item):
+        self.items.append(item)
+
+    def popFront(self):
+        if self.empty():
+            raise Exception("Queue: 'popFront' applied to empty container")
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+def BFS(file):
+    q = Queue()
+    q.pushBack(file)
+    data = {}
+
+    while not q.empty():
+        current = q.popFront()
+
+        if isinstance(current, h5py.Dataset):
+            try:
+                data[current.name] = ['dataset', current.size, current.shape, current.dtype]
+            # TypeError: No NumPy equivalent for TypeBitfieldID exists
+            except TypeError:
+                data[current.name] = ['dataset', current.size, current.shape, None]
+        else:
+            data[current.name] = ['group', None, None, None]
+            if isinstance(current, h5py.Group):
+                [q.pushBack(current[key]) for key in list(current)]
+            # Datatype object
+            else:
+                data[current.name] = ['group', None, None, current.dtype]
+
+    return data
+
+with h5py.File(args.input, 'r') as file:
+    data = BFS(file)
+    
+    df = pd.DataFrame.from_dict(data, orient='index', columns=['class','size','shape','dtype'])
+    df.to_csv('traverse_directory_tree.csv')
